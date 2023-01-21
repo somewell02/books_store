838c961170a15dfb4ca7506ff4207210a7729475
Samvel Grigoryan <somewell1212@gmail.com>
2023-01-22 00:13:28 +0300

    Added firebase config

---
+++ src/data/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyAvS6WvcnEqq0lmCBc1H3LKsvOoyG23sMU",
+  authDomain: "book-store-d36d0.firebaseapp.com",
+  projectId: "book-store-d36d0",
+  storageBucket: "book-store-d36d0.appspot.com",
+  messagingSenderId: "128637510118",
+  appId: "1:128637510118:web:4c6aa50107a7bf02857627",
+  measurementId: "G-MZKQFNP7QS",
+};
